40292a70e31e6197f88806f9d043b7028e3e3f4f
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2025-04-06 13:34:51 +0700

    Fixed swagger

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,38 @@ async function bootstrap() {
     .addBearerAuth()
     .addSecurityRequirements('bearer')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, documentFactory);
+    const document = SwaggerModule.createDocument(app, config);
+    // Serve raw OpenAPI JSON
+  app.use('/api/swagger-json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(document);
+  });
+
+  // Serve Swagger UI HTML (from CDN)
+  app.use('/api/swagger', (req, res) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>API Docs</title>
+          <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+        </head>
+        <body>
+          <div id="swagger-ui"></div>
+          <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+          <script>
+            window.onload = function () {
+              SwaggerUIBundle({
+                url: '/api/swagger-json',
+                dom_id: '#swagger-ui',
+              });
+            };
+          </script>
+        </body>
+      </html>
+    `);
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
